c1c1bc4b38d2677eff2f6d4ea6e6703269f74bd1
Mgazul <mgazul@163.com>
2021-06-12 21:13:12 +0800

    rename

--- src/main/java/com/mohistmc/luyten/MainMenuBar.java
+++ src/main/java/com/mohistmc/luyten/MainMenuBar.java
@@ -580,7 +580,7 @@ public class MainMenuBar extends JMenuBar {
 				link.setCursor(new Cursor(Cursor.HAND_CURSOR));
 				link.addMouseListener(new LinkListener(rsyntax, link));
 				pane.add(link);
-				pane.add(new JLabel("Version: 3.0.4"));
+				pane.add(new JLabel("Version: 3.1.2"));
 				pane.add(new JLabel("(c) 2019 Robert Futrell"));
 				pane.add(new JLabel(" "));
 				JOptionPane.showMessageDialog(null, pane);
